f29a3f93cc51edd2bbd34c2d5c2e0a7f2ce3c422
Damian Święcki <damian.swiecki@snowflake.com>
2024-01-17 12:22:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit dswiecki3 2023-10-31 16:43! 
+# Welcome to Streamlit dswiecki3 2024-01-17 12:21! 
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
